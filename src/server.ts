f3f7791b037ff5d7aaa095f408f5f2f95e773396
delta-kor <deltakbot1@gmail.com>
2020-08-10 08:24:00 +0900

    Add route

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,29 @@
 import * as express from 'express';
+import { router as ApiRouter } from './routes/api';
+import { router as WebRouter } from './routes/web';
 
 export class Server {
 
     public application: express.Application;
     public port: number;
 
-    constructor(port: number = 30000) {
+    constructor(port: number = 3000) {
         this.application = express();
         this.port = port;
+        this.mount();
+    }
+
+    private mount() {
+        this.mountRoutes();
+    }
+
+    private mountRoutes() {
+        this.application.use('/api', ApiRouter);
+        this.application.use(WebRouter);
     }
 
     listen(onStart?: (port: number) => void): void {
-        this.application.listen(onStart);
+        this.application.listen(this.port, onStart);
     }
 
 }
