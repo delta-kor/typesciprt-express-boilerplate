866f676a889eefe14b8ca067f6766a873b7e306d
delta-kor <deltakbot1@gmail.com>
2020-08-12 09:17:47 +0900

    Fix menu controller

--- src/controllers/web/menu.controller.ts
+++ src/controllers/web/menu.controller.ts
@@ -6,6 +6,7 @@ export class MenuController {
     static todo(req: Request, res: Response): any {
         const todos: Todo[] = todoService.getTodos();
         res.render('todo.pug', { todos });
+        return true;
     }
 
 }
